a560a8a96ce4d9d84b74803851a6cddd6fd06665
Odoomates <odoomates@gmail.com>
2019-02-26 22:54:25 +0530

    [ADD] Stock forecast module: Initial commit

---
+++ stock_forecast/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Stock Forecast',
+    'version': '11.0.1.0.0',
+    'summary': 'Stock Forecast Analysis',
+    'author': 'Odoo Mates, Odoo SA',
+    'company': 'Odoo Mates',
+    'maintainer': 'Odoo Mates',
+    'category': 'Inventory',
+    'depends': ['stock'],
+    'data': [
+          'views/stock_forecast.xml',
+    ],
+    'license': 'AGPL-3',
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
