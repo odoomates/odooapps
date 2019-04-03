b7c708482dddfd567ff093b15014da85ec1ef86d
Odoomates <odoomates@gmail.com>
2019-04-03 23:19:58 +0530

    [ADD] Initial Commit

---
+++ payment_razorpay/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Razorpay Payment Acquirer',
+    'category': 'Accounting',
+    'summary': 'Payment Acquirer: Razorpay Implementation',
+    'version': '1.0',
+    'author': 'Odoo Mates, Odoo SA',
+    'description': """Razorpay Payment Acquirer""",
+    'depends': ['payment'],
+    'live_test_url': 'https://www.youtube.com/watch?v=Zg0EzM-ogQU',
+    'application': True,
+    'data': [
+        'views/payment_views.xml',
+        'views/payment_razorpay_templates.xml',
+        'data/payment_acquirer_data.xml',
+    ],
+    'post_init_hook': 'create_missing_journal_for_acquirers',
+    'images': ['static/description/banner.png'],
+}
