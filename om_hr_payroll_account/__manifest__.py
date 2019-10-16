2329c3c380a7380385be83da0b6bc144078ac770
dell <odoomates@gmail.com>
2019-10-16 14:44:06 +0530

    [ADD] Hr payroll & payroll accounting in v13

---
+++ om_hr_payroll_account/__manifest__.py
@@ -0,0 +1,20 @@
+#-*- coding:utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Odoo13 Payroll Accounting',
+    'version': '13.0.1.0.0',
+    'category': 'Human Resources',
+    'author': 'Odoo Mates, Odoo SA',
+    'description': """
+Generic Payroll system Integrated with Accounting.
+==================================================
+
+    * Expense Encoding
+    * Payment Encoding
+    * Company Contribution Management
+    """,
+    'depends': ['om_hr_payroll', 'account'],
+    'data': ['views/hr_payroll_account_views.xml'],
+    'demo': ['data/hr_payroll_account_demo.xml'],
+    'test': ['../account/test/account_minimal_test.xml'],
+}
