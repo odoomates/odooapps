857817f2e9e193de8665ea8a64fa56b086bf1611
odoomates <odoomates@gmail.com>
2019-11-08 10:02:07 +0530

    [FIX] General Ledger Typo

--- accounting_pdf_reports/__manifest__.py
+++ accounting_pdf_reports/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Odoo 13 Accounting PDF Reports',
-    'version': '13.0.1.0.0',
+    'version': '13.0.1.0.1',
     'category': 'Invoicing Management',
     'summary': 'Accounting Reports For Odoo 13',
     'sequence': '10',
