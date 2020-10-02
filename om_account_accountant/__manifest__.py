483830f6a7607a18a955b2f4113b410d32f3d264
odoomates <odoomates@gmail.com>
2020-10-02 13:29:31 +0300

    [ADD] Odoo14 accounting

---
+++ om_account_accountant/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Odoo 14 Accounting',
+    'version': '14.0.2.0.2',
+    'category': 'Accounting',
+    'summary': 'Accounting Reports, Asset Management and Account Budget For Odoo14 Community Edition',
+    'live_test_url': 'https://www.youtube.com/watch?v=Kj4hR7_uNs4',
+    'sequence': '8',
+    'website': 'http://odoomates.tech',
+    'author': 'Odoo Mates, Odoo SA',
+    'maintainer': 'Odoo Mates',
+    'license': 'LGPL-3',
+    'support': 'odoomates@gmail.com',
+    'website': '',
+    'depends': ['accounting_pdf_reports', 'om_account_asset', 'om_account_budget'],
+    'demo': [],
+    'data': [
+        'views/account.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': ['static/description/banner.png'],
+    'qweb': [],
+}
---
+++ om_account_accountant/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import account_fiscal_year
+from . import account_settings
