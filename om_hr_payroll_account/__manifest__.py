4c792484a8e351fe3b140b08314a5e8734036983
odoomates <odoomates@gmail.com>
2020-10-03 20:50:44 +0300

    [ADD] Odoo 14 HR Payroll Accounting

---
+++ om_hr_payroll_account/__manifest__.py
@@ -0,0 +1,23 @@
+#-*- coding:utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Odoo 14 Payroll Accounting',
+    'category': 'Human Resources',
+    'author': 'Odoo Mates, Odoo SA',
+    'version': '14.0.1.0.0',
+    'website': 'http://odoomates.tech',
+    'description': """
+Generic Payroll system Integrated with Accounting.
+==================================================
+
+    * Expense Encoding
+    * Payment Encoding
+    * Company Contribution Management
+    """,
+    'depends': ['om_hr_payroll', 'account'],
+    'data': ['views/hr_payroll_account_views.xml'],
+    'demo': [],
+    'test': ['../account/test/account_minimal_test.xml'],
+    'images': ['static/description/banner.png'],
+}
