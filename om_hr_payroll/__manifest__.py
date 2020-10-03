8352a21faeb3dd071bf17dc7f50c7e26aec44c73
odoomates <odoomates@gmail.com>
2020-10-03 20:46:30 +0300

    [ADD] Odoo 14 HR Payroll

---
+++ om_hr_payroll/__manifest__.py
@@ -0,0 +1,36 @@
+#-*- coding:utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Odoo 14 Payroll',
+    'category': 'Human Resources',
+    'version': '14.0.2.1.0',
+    'sequence': 1,
+    'author': 'Odoo Mates, Odoo SA',
+    'summary': 'Payroll For Odoo 14 Community Edition',
+    'description': "",
+    'website': 'https://www.odoomates.tech',
+    'depends': [
+        'hr_contract',
+        'hr_holidays',
+    ],
+    'data': [
+        'security/hr_payroll_security.xml',
+        'security/ir.model.access.csv',
+        'wizard/hr_payroll_payslips_by_employees_views.xml',
+        'views/hr_contract_views.xml',
+        'views/hr_salary_rule_views.xml',
+        'views/hr_payslip_views.xml',
+        'views/hr_employee_views.xml',
+        'data/hr_payroll_sequence.xml',
+        'views/hr_payroll_report.xml',
+        'data/hr_payroll_data.xml',
+        'wizard/hr_payroll_contribution_register_report_views.xml',
+        'views/res_config_settings_views.xml',
+        'views/report_contributionregister_templates.xml',
+        'views/report_payslip_templates.xml',
+        'views/report_payslipdetails_templates.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'application': True,
+}
