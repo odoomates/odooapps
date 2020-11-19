e22c10643f042007830be650142ee6ccb4c83973
odoomates <odoomates@gmail.com>
2020-11-19 11:53:42 +0300

    [FIX] Asset Error

--- om_account_asset/__manifest__.py
+++ om_account_asset/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Odoo 14 Assets Management',
-    'version': '14.0.1.4.0',
+    'version': '14.0.1.5.0',
     'author': 'Odoo Mates, Odoo SA',
     'depends': ['account'],
     'description': """Manage assets owned by a company or a person. 
