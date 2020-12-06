ba86271805357d41242009986bb6b9a73ec4cbe7
odoomates <odoomates@gmail.com>
2020-12-06 22:30:54 +0300

    [UPDT] Payroll

--- om_hr_payroll/models/res_config_settings.py
+++ om_hr_payroll/models/res_config_settings.py
@@ -6,5 +6,5 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    module_om_hr_payroll_account = fields.Boolean(string='Payroll Accountant')
+    module_om_hr_payroll_account = fields.Boolean(string='Payroll Accounting')
 
