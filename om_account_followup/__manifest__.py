19e3990b43d251b97b1a3c26e14231818f555662
odoomates <odoomates@gmail.com>
2021-02-06 22:32:05 +0300

    [ADD] Customer Follow Up

---
+++ om_account_followup/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Customer Payment FollowUp Management',
+    'version': '14.0.1.0.0',
+    'category': 'Accounting',
+    'description': """Customer Payment FollowUp Management""",
+    'summary': """Customer Payment FollowUp Management""",
+    'author': 'Odoo Mates, Odoo S.A, OdooDev',
+    'license': 'LGPL-3',
+    'website': '',
+    'depends': ['account', 'mail'],
+    'data': [
+        'security/account_followup_security.xml',
+        'security/ir.model.access.csv',
+        'data/data.xml',
+        'wizard/followup_print_view.xml',
+        'wizard/followup_results_view.xml',
+        'views/followup_view.xml',
+        'views/account_move.xml',
+        'views/partners.xml',
+        'views/report_followup.xml',
+        'views/reports.xml',
+        'views/followup_partner_view.xml',
+        'report/followup_report.xml',
+    ],
+    'demo': ['demo/demo.xml'],
+    'images': [],
+    'installable': True,
+    'auto_install': False,
+}
