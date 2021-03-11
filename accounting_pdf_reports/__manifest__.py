29555caec8403b97507f8500f1b7b2e046716e36
odoomates <odoomates@gmail.com>
2021-03-11 21:21:51 +0300

    [IMP] Translation

--- accounting_pdf_reports/__manifest__.py
+++ accounting_pdf_reports/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Odoo 12 Accounting PDF Reports',
-    'version': '12.0.1.0.0',
+    'version': '12.0.2.0.0',
     'category': 'Invoicing Management',
     'summary': 'Accounting Reports For Odoo 12',
     'sequence': '10',
