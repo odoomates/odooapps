d8a33707e7d6480919aac2a9be58f0154e3af618
odoomates <odoomates@gmail.com>
2021-07-10 21:49:53 +0300

    [ADD] Daily Reports

---
+++ om_account_daily_reports/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Cash Book, Day Book, Bank Book Financial Reports',
+    'version': '14.0.1.0.0',
+    'category': 'Invoicing Management',
+    'summary': 'Cash Book, Day Book and Bank Book Report For Odoo 14',
+    'description': 'Cash Book, Day Book and Bank Book Report For Odoo 14',
+    'sequence': '10',
+    'author': 'Odoo Mates, Odoo SA',
+    'license': 'LGPL-3',
+    'company': 'Odoo Mates',
+    'maintainer': 'Odoo Mates',
+    'support': 'odoomates@gmail.com',
+    'website': 'https://www.odoomates.tech',
+    'depends': ['account'],
+    'live_test_url': '',
+    'demo': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizards/daybook.xml',
+        'wizards/cashbook.xml',
+        'wizards/bankbook.xml',
+        'views/om_daily_reports.xml',
+        'reports/reports.xml',
+        'reports/report_daybook.xml',
+        'reports/report_cashbook.xml',
+        'reports/report_bankbook.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'images': ['static/description/banner.gif'],
+    'qweb': [],
+}
---
+++ om_account_daily_reports/wizards/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import account_daybook_report
+from . import account_cashbook_report
+from . import accoun_bankbook_report
+
