53a3e3a7f8fc7ca8275b6603f0feaa25212fe1dc
odoomates <odoomates@gmail.com>
2021-10-14 05:33:15 +0300

    [ADD] Odoo 15 bank statement import

---
+++ om_account_bank_statement_import/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'Odoo 15 Account Bank Statement Import',
+    'version': '15.0.1.0.0',
+    'category': 'Accounting',
+    'depends': ['account'],
+    'website': 'https://www.odoomates.tech',
+    'author': 'Odoo Mates, Odoo SA',
+    'support': 'odoomates@gmail.com',
+    'maintainer': 'Odoo Mates',
+    'license': 'LGPL-3',
+    'description': """Generic Wizard to Import Bank Statements In Odoo15 Community Edition.
+(This module does include any CSV and XLSX type import format.)""",
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/journal_creation.xml',
+        'views/account_bank_statement_import_view.xml',
+        'views/account_bank_statement_import_templates.xml',
+        'views/res_config_settings_views.xml',
+    ],
+    'demo': [
+        'demo/partner_bank.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': True,
+}
