ca35bf0d422a575189bda5ca13247ae956c1a153
odoomates <odoomates@gmail.com>
2021-10-14 05:35:14 +0300

    [ADD] Odoo 15 Accountinh

---
+++ om_account_accountant/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Odoo 15 Accounting',
+    'version': '15.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Accounting Reports, Asset Management and Account Budget For Odoo15 Community Edition',
+    'live_test_url': 'https://www.youtube.com/watch?v=Kj4hR7_uNs4',
+    'sequence': '1',
+    'website': 'https://www.odoomates.tech',
+    'author': 'Odoo Mates, Odoo SA',
+    'maintainer': 'Odoo Mates',
+    'license': 'LGPL-3',
+    'support': 'odoomates@gmail.com',
+    'depends': ['accounting_pdf_reports',
+                'om_account_asset',
+                'om_account_budget',
+                'om_fiscal_year',
+                'om_recurring_payments',
+                'om_account_bank_statement_import'
+                ],
+    'demo': [],
+    'data': [
+        'views/settings.xml',
+        'views/account_type.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': ['static/description/banner.png'],
+    'qweb': [],
+}
---
+++ om_account_accountant/doc/index.rst
@@ -0,0 +1,46 @@
+==================
+Odoo 15 Accounting
+==================
+
+This Module will bring back the accounting features such as Account Reports, Asset Management
+and Budget Management into Odoo 15 Community Edition
+
+Installation
+============
+
+To install this module, you need to:
+
+Download the module and add it to your Odoo addons folder. Afterward, log on to
+your Odoo server and go to the Apps menu. Trigger the debug mode and update the
+list by clicking on the "Update Apps List" link. Now install the module by
+clicking on the install button.
+
+Upgrade
+============
+
+To upgrade this module, you need to:
+
+Download the module and add it to your Odoo addons folder. Restart the server
+and log on to your Odoo server. Select the Apps menu and upgrade the module by
+clicking on the upgrade button.
+
+
+Configuration
+=============
+
+There is Nothing to Configure
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Odoo Mates <odoomates@gmail.com>
+
+
+Author & Maintainer
+-------------------
+
+This module is maintained by the Odoo Mates
