1cf413266228a786d3a37f47083b396952a2799f
odoomates <odoomates@gmail.com>
2021-11-30 20:21:40 +0300

    [ADD] Zatca invoice report

---
+++ om_gcc_invoice/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'GCC - Arabic/English Invoice',
+    'version': '1.0.0',
+    'author': 'Odoo Mates, Odoo SA',
+    'category': 'Accounting/Localizations',
+    'description': """Arabic/English for GCC""",
+    'license': 'LGPL-3',
+    'depends': ['account'],
+    'data': [
+        'views/report_invoice.xml',
+    ],
+    'images': ['static/description/banner.png'],
+}
---
+++ om_sa_invoice/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'KSA E-Invoicing | Zatca Invoicing',
+    'version': '1.0.0',
+    'author': 'Odoo Mates, Odoo SA',
+    'category': 'Accounting/Localizations',
+    'license': 'LGPL-3',
+    'summary': """Invoices for the Kingdom of Saudi Arabia""",
+    'description': """Electronic invoice KSA - POS Encoded | qrcode | ZATCA | vat | e-invoice | tax | Zakat""",
+    'depends': ['l10n_sa', 'om_gcc_invoice'],
+    'data': [
+        'views/view_move_form.xml',
+        'views/report_invoice.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'live_test_url': 'https://www.youtube.com/watch?v=cTE4T31qhvo&t=1s'
+}
