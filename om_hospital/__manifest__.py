9b91e00d6082960b792b0130728a1d4d90754354
odoomates <odoomates@gmail.com>
2021-12-14 07:23:48 +0300

    [IMP] om_hospital

--- om_hospital/__manifest__.py
+++ om_hospital/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Odoo 15 Development Tutorials',
-    'version': '1.0.0',
+    'version': '2.0.0',
     'summary': 'Odoo 15 Development Tutorials',
     'sequence': -100,
     'description': """Odoo 15 Development Tutorials""",
