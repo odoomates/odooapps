1cbe754cd0bbef74edd4fe8aa254958e8a98d9fd
odoomates <odoomates@gmail.com>
2022-02-10 21:35:06 +0300

    [IMP] credit limit

--- om_credit_limit/models/account_move.py
+++ om_credit_limit/models/account_move.py
@@ -8,12 +8,15 @@ class AccountMove(models.Model):
     partner_credit_limit = fields.Monetary(related='partner_id.credit_limit_compute', readonly=True)
     show_partner_credit_warning = fields.Boolean(compute='_compute_show_partner_credit_warning')
 
-    @api.depends('partner_credit_limit', 'partner_credit', 'company_id.account_default_credit_limit')
+    @api.depends('partner_credit_limit', 'partner_credit',
+                 'company_id.account_default_credit_limit', 'company_id.account_credit_limit')
     def _compute_show_partner_credit_warning(self):
         for move in self:
+            account_credit_limit = move.company_id.account_credit_limit
             company_limit = move.partner_credit_limit == -1 and move.company_id.account_default_credit_limit
             partner_limit = move.partner_credit_limit > 0 and move.partner_credit_limit
-            move.show_partner_credit_warning = (company_limit and move.partner_credit > company_limit) or \
-                                               (partner_limit and move.partner_credit > partner_limit)
+            move.show_partner_credit_warning = account_credit_limit and \
+                                               ((company_limit and move.partner_credit > company_limit) or \
+                                               (partner_limit and move.partner_credit > partner_limit))
 
 
