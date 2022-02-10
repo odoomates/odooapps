5b5ba5a251dc3aa9048cc4470156220d2b3fb631
odoomates <odoomates@gmail.com>
2022-02-10 19:37:34 +0300

    [IMP] credit limit

--- om_credit_limit/__manifest__.py
+++ om_credit_limit/__manifest__.py
@@ -2,17 +2,18 @@
     'name': 'Odoo 15 Credit Limit',
     'author': 'Odoo Mates',
     'category': 'Accounting',
-    'version': '1.0.0',
+    'version': '2.0.0',
     'description': """Customer Credit Limit""",
     'summary': """Customer Credit Limit""",
     'sequence': 11,
     'website': 'https://www.odoomates.tech',
-    'depends': ['account'],
+    'depends': ['account', 'sale'],
     'license': 'LGPL-3',
     'data': [
-        'security/ir.model.access.csv',
-        'wizard/warning.xml',
-        'views/res_partner.xml'
+        'views/res_partner.xml',
+        'views/account_move.xml',
+        'views/sale_order.xml',
+        'views/res_config_settings.xml',
     ],
     'images': ['static/description/banner.png'],
 
