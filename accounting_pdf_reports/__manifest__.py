3daf78864eac3fa8daae5c9f392922c0aa77b819
odoomates <odoomates@gmail.com>
2022-02-22 21:40:09 +0300

    [FIX] aged partner

--- accounting_pdf_reports/__manifest__.py
+++ accounting_pdf_reports/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Odoo 15 Accounting Financial Reports',
-    'version': '15.0.6.1.0',
+    'version': '15.0.6.2.0',
     'category': 'Invoicing Management',
     'description': 'Accounting Reports For Odoo 15, Accounting Financial Reports, Odoo 15 Financial Reports',
     'summary': 'Accounting Reports For Odoo 15',
--- accounting_pdf_reports/doc/RELEASE_NOTES.md
+++ accounting_pdf_reports/doc/RELEASE_NOTES.md
@@ -1,5 +1,10 @@
 ## Module <accounting_pdf_reports>
 
+#### 22.02.2022
+#### Version 15.0.6.2.0
+##### FIX
+- aged partner with respect to changes in odoo core accounting
+
 #### 01.02.2022
 #### Version 15.0.6.1.0
 ##### IMP
