9eba183e2d4c954ecc757e37644b602c792a5456
odoomates <odoomates@gmail.com>
2022-02-26 09:08:54 +0300

    [IMP] pre init hook

--- accounting_pdf_reports/__init__.py
+++ accounting_pdf_reports/__init__.py
@@ -5,4 +5,4 @@ from . import reports
 
 
 def _pre_init_clean_m2m_models(cr):
-    cr.execute("""DROP TABLE account_journal_account_report_partner_ledger_rel""")
+    cr.execute("""DROP TABLE IF EXISTS account_journal_account_report_partner_ledger_rel""")
