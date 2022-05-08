ad85f2aba4309727cc7aa7fc18fac7c3b2d0ac9c
odoomates <odoomates@gmail.com>
2022-05-08 21:37:23 +0300

    [IMP] credit limit

--- om_credit_limit/models/account_move.py
+++ om_credit_limit/models/account_move.py
@@ -10,22 +10,23 @@ class AccountMove(models.Model):
     show_partner_credit_warning = fields.Boolean(compute='_compute_show_partner_credit_warning')
     credit_limit_type = fields.Selection(related='company_id.credit_limit_type')
 
-    @api.depends('partner_credit_limit', 'partner_credit',
+    @api.depends('partner_credit_limit', 'partner_credit', 'invoice_line_ids',
                  'company_id.account_default_credit_limit', 'company_id.account_credit_limit')
     def _compute_show_partner_credit_warning(self):
         for move in self:
             account_credit_limit = move.company_id.account_credit_limit
             company_limit = move.partner_credit_limit == -1 and move.company_id.account_default_credit_limit
             partner_limit = move.partner_credit_limit > 0 and move.partner_credit_limit
+            partner_credit = move.partner_credit + move.amount_total
             move.show_partner_credit_warning = account_credit_limit and \
-                                               ((company_limit and move.partner_credit > company_limit) or \
-                                               (partner_limit and move.partner_credit > partner_limit))
+                                               ((company_limit and partner_credit > company_limit) or \
+                                               (partner_limit and partner_credit > partner_limit))
 
     def action_post(self):
         result = super(AccountMove, self).action_post()
         for inv in self:
             if inv.show_partner_credit_warning and inv.credit_limit_type == 'block' and \
-                    inv.partner_credit > inv.partner_credit_limit:
+                    inv.partner_credit + inv.amount_total > inv.partner_credit_limit:
                 raise ValidationError(_("You cannot exceed credit limit !"))
         return result
 
