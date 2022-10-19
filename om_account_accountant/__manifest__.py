d61b0b8c5f8f1ca564586c21fc8533f11f5e93c0
odoomates <odoomates@gmail.com>
2022-10-19 16:55:28 +0300

    [IMP] index file

--- om_account_accountant/__manifest__.py
+++ om_account_accountant/__manifest__.py
@@ -47,6 +47,6 @@
     'installable': True,
     'application': True,
     'auto_install': False,
-    'images': ['static/description/banner.gif'],
+    'images': ['static/description/banner.png'],
 }
 
