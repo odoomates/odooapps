c8d298828c5e9868e790033472d9ef2a1dcf754a
odoomates <odoomates@gmail.com>
2024-01-31 21:26:23 +0300

    [FIX] om_fiscal_year: traceback on accessing settings page
    before this commit, accessing the settings page in debug
    mode raises traceback.
    
    after this commit, the traceback is resolved and settings
    page can be accessed in debug mode

--- om_fiscal_year/__manifest__.py
+++ om_fiscal_year/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Odoo 17 Fiscal Year & Lock Date',
-    'version': '17.0.1.1',
+    'version': '17.0.1.2',
     'category': 'Accounting',
     'summary': 'Odoo 17 Fiscal Year, Fiscal Year in Odoo 17, Lock Date in Odoo 17',
     'description': 'Odoo 17 Fiscal Year, Fiscal Year in Odoo 17',
