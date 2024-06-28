d91a1566655b085e49c2ca277f2e08300042886c
odoomates <odoomates@gmail.com>
2024-06-28 16:24:48 +0300

    [FIX] om_data_remove: correct the version in index file

--- om_data_remove/__manifest__.py
+++ om_data_remove/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Odoo 17 Remove Data',
-    'version': '17.0.1.0.1',
+    'version': '17.0.1.0.2',
     'author': 'Odoo Mates, Sunpop.cn',
     'category': 'Tools',
     'description': 'Data Clean up, Remove Data, Database Clean UP, Reset Database',
