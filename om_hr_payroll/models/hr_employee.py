9bb6c6b121a9e148a0f0bea035e99502e7ae458b
SaiyedMahediAbbas <saiyedmehdiabbas8@gmail.com>
2026-01-31 22:36:54 +0530

    Add and Bug Fix Payroll Module

--- om_hr_payroll/models/hr_employee.py
+++ om_hr_payroll/models/hr_employee.py
@@ -8,7 +8,12 @@ class HrEmployee(models.Model):
     slip_ids = fields.One2many('hr.payslip', 'employee_id', string='Payslips', readonly=True)
     payslip_count = fields.Integer(compute='_compute_payslip_count', string='Payslip Count',
                                    groups="om_om_hr_payroll.group_hr_payroll_user")
-    current_contract_struct_id = fields.Many2one(related="current_version_id.struct_id", string='Salary Structure')
+    struct_id = fields.Many2one(
+        related="current_version_id.struct_id",
+        string='Salary Structure',
+        store=True,
+        readonly=False
+    )
     def _compute_payslip_count(self):
         for employee in self:
             employee.payslip_count = len(employee.slip_ids)
